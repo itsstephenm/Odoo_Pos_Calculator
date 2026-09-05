8face541d025423792a726a1587ec01b80727cc9
Stephen <muregwistephen1@gmail.com>
2026-09-05 23:33:02 +0200

    Add safe DebugWidget getter patch to prevent _role crash

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -2,11 +2,26 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { DebugWidget } from "@point_of_sale/app/utils/debug/debug_widget";
 import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
 
 // Patch the Calculator Action into ControlButtons
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
+    }
+});
+
+// Safely patch DebugWidget to prevent crashes when cashier is unassigned
+patch(DebugWidget.prototype, {
+    get isDisabled() {
+        if (!this.pos?.cashier) {
+            return true;
+        }
+        try {
+            return super.isDisabled;
+        } catch (e) {
+            return false;
+        }
     }
 });
