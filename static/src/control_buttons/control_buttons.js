89c6745914f2c9f4c2b81aaac036fb0f784bff0b
Stephen <muregwistephen1@gmail.com>
2026-09-05 22:48:38 +0200

    Strip harmful debug patches and keep only calculator button injection

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -3,18 +3,10 @@
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { CalculatorDialog } from "@Odoo_Pos_Calculator/calculator_dialog/calculator_dialog";
-import { DebugWidget } from "@point_of_sale/app/utils/debug/debug_widget";
 
 // Patch the Calculator Action into ControlButtons
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
-    }
-});
-
-// Fix Odoo 19 crash: Safely fallback using optional chaining if cashier session is uninitialized
-patch(DebugWidget.prototype, {
-    get isDisabled() {
-        return this.pos?.cashier?._role === "minimal";
     }
 });
