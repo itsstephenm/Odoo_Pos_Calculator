b3e89c076c58dbe3c80923dfa0b722d67356f710
Stephen <muregwistephen1@gmail.com>
2026-09-06 00:02:28 +0200

    Remove all experimental patches, keep only clean button injection

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -2,24 +2,10 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
-import { DebugWidget } from "@point_of_sale/app/utils/debug/debug_widget";
 import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
 
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
-    }
-});
-
-patch(DebugWidget.prototype, {
-    get isDisabled() {
-        try {
-            if (!this.pos?.cashier) {
-                return true;
-            }
-            return super.isDisabled;
-        } catch (e) {
-            return true;
-        }
     }
 });
