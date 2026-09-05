09abe5573a01eb0a817f8913b3eb170da58f4a58
Stephen <muregwistephen1@gmail.com>
2026-09-05 23:49:37 +0200

    Add safe DebugWidget guard to prevent core Odoo _role crash

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -2,10 +2,26 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { DebugWidget } from "@point_of_sale/app/utils/debug/debug_widget";
 import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
 
+// 1. Inject the Calculator button
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
+    }
+});
+
+// 2. Protect core Odoo DebugWidget from crashing when cashier is undefined
+patch(DebugWidget.prototype, {
+    get isDisabled() {
+        if (!this.pos || !this.pos.cashier) {
+            return true;
+        }
+        try {
+            return super.isDisabled;
+        } catch (e) {
+            return false;
+        }
     }
 });
