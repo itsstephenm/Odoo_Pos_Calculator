0dc89808800659e7425ed76a05191b29dcc1051c
Stephen <muregwistephen1@gmail.com>
2026-09-05 23:26:38 +0200

    Fix JS import to use lowercase pos_calculator namespace

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -1,8 +1,8 @@
-/** @odoo-module **/
+/** @odoo-module */
 
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
-import { CalculatorDialog } from "@Odoo_Pos_Calculator/calculator_dialog/calculator_dialog";
+import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
 
 // Patch the Calculator Action into ControlButtons
 patch(ControlButtons.prototype, {
