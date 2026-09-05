3debe82fd5242d439882a83e1aa991da99f8bc90
Stephen <muregwistephen1@gmail.com>
2026-09-06 00:10:37 +0200

    Add safe getNumpadButtons getter guard to prevent _role crash

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -2,10 +2,23 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
 import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
 
+// Inject the Calculator button into control buttons
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
+    }
+});
+
+// Guard ProductScreen numpad rendering against undefined role states
+patch(ProductScreen.prototype, {
+    get getNumpadButtons() {
+        try {
+            return super.getNumpadButtons;
+        } catch (e) {
+            return {};
+        }
     }
 });
