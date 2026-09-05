6119f0c6d6c7ac2267e96a3b986cfe13cd54e26d
Stephen <muregwistephen1@gmail.com>
2026-09-06 00:26:44 +0200

    Add safe getNumpadButtons fallback wrapper to prevent cashier role crash

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -12,13 +12,18 @@ patch(ControlButtons.prototype, {
     }
 });
 
-// Guard ProductScreen numpad rendering against undefined role states
+// Guard ProductScreen numpad rendering against undefined cashier/role states
 patch(ProductScreen.prototype, {
-    get getNumpadButtons() {
+    getNumpadButtons() {
         try {
-            return super.getNumpadButtons;
+            return super.getNumpadButtons(...arguments);
         } catch (e) {
-            return {};
+            return [
+                { value: "1" }, { value: "2" }, { value: "3" },
+                { value: "4" }, { value: "5" }, { value: "6" },
+                { value: "7" }, { value: "8" }, { value: "9" },
+                { value: "0" }, { value: "." }, { value: "-" }
+            ];
         }
     }
 });
