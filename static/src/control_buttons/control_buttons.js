e0187fd44025f014e8d4eb09a83267d7c1a5a0f1
Stephen <muregwistephen1@gmail.com>
2026-09-08 10:39:35 +0200

    Remove obsolete pos_hr OrderSummary patch to fix undefined prototype crash

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -3,24 +3,9 @@
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
-import { OrderSummary } from "@pos_hr/app/screens/product_screen/order_summary/order_summary";
 
 patch(ControlButtons.prototype, {
     onClickCalculator() {
         this.dialog.add(CalculatorDialog, {});
-    }
-});
-
-// Guard pos_hr OrderSummary against unassigned cashier roles (fixes the missing delete button and layout crash)
-patch(OrderSummary.prototype, {
-    updateSelectedOrderline() {
-        if (!this.pos?.cashier) {
-            return;
-        }
-        try {
-            return super.updateSelectedOrderline(...arguments);
-        } catch (e) {
-            return;
-        }
     }
 });
