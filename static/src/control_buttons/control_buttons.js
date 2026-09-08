db7f47ac6534ed6661df5c952406a4b3370fae17
Stephen <muregwistephen1@gmail.com>
2026-09-08 14:52:30 +0200

    Include patch utility import in control_buttons.js

--- static/src/control_buttons/control_buttons.js
+++ static/src/control_buttons/control_buttons.js
@@ -1,3 +1,30 @@
+/** @odoo-module */
+
+import { patch } from "@web/core/utils/patch";
+import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { DebugWidget } from "@point_of_sale/app/utils/debug/debug_widget";
+import { LoginScreen } from "@point_of_sale/app/screens/login_screen/login_screen";
+import { CalculatorDialog } from "@pos_calculator/calculator_dialog/calculator_dialog";
+
+patch(ControlButtons.prototype, {
+    onClickCalculator() {
+        this.dialog.add(CalculatorDialog, {});
+    }
+});
+
+patch(DebugWidget.prototype, {
+    get isDisabled() {
+        if (!this.pos || !this.pos.cashier) {
+            return true;
+        }
+        try {
+            return super.isDisabled;
+        } catch (e) {
+            return true;
+        }
+    }
+});
+
 patch(LoginScreen.prototype, {
     clickBack() {
         try {
